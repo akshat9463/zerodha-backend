b8c51b1913b314379866cf3653bda812d39f8f88
akshat9463 <akshatkhadi@gmail.com>
2025-07-12 14:26:59 +0530

    responsiveness

--- models/userModel.js
+++ models/userModel.js
@@ -1,28 +1,7 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const {model} = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: [true, "Your email address is required"],
-    unique: true,
-  },
-  username: {
-    type: String,
-    required: [true, "Your username is required"],
-  },
-  password: {
-    type: String,
-    required: [true, "Your password is required"],
-  },
-  createdAt: {
-    type: Date,
-    default: new Date(),
-  },
-});
+const {userSchema} = require('../schemas/userSchema');
 
-userSchema.pre("save", async function () {
-  this.password = await bcrypt.hash(this.password, 12);
-});
+const UserModel = new model('User', userSchema);
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = {UserModel};
